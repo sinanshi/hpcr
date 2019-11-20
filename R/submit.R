46a41945baf4ad624b2e87d6aebf1d1393a73ca9
Sinan Shi <shisinancn@gmail.com>
2019-11-20 00:16:11 +0000

    adding slurm submit

--- R/submit.R
+++ R/submit.R
@@ -18,3 +18,50 @@ get_name_from_path <- function(path) {
   name <- str_split(path, "/")[[1]]
   return(name[length(name)])
 }
+
+
+#' Create script from template
+create_script <- function(cmd, job_name, log_path, nnodes=1, nproc=1, 
+                          timelim="480:00:00", template="slurm") {
+  param <- list()
+  param[["job_name"]] <- job_name
+  param[["log_path"]] <- log_path
+  param[["nnodes"]] <- nnodes
+  param[["nproc"]] <- nproc
+  param[["timelim"]] <- timelim
+  param[["cmd"]] <- cmd
+  script <- readLines(system.file(template, package="hpcr"))
+  script <- whisker.render(script, param)
+  return(script)
+}
+
+
+slurm_job <- function(script, job_name, log_path=".", 
+                         nnodes=1, nproc=1, timelim="48:00:00", 
+                         verbose=FALSE) {
+  script <- create_script(cmd=script, job_name=job_name, 
+                          log_path=log_path, 
+                          nnodes=nnodes, nproc=nproc,
+                          timelim=timelim,
+                          template="slurm")
+  file <- tempfile()
+  writeLines(script, file)
+  if (verbose) {
+    f <- readLines(file)
+    cat(f, sep="\n")
+  }
+
+  system2("sbatch", file)
+}
+
+#' @export
+submit_slurm <- function(scirpt, param, job_name, log_path, 
+                         nnodes=1, nproc=1, timelim="48:00:00", 
+                         verbose=FALSE) {
+  for (i in seq(param)) {
+    p <- as.list(param[i, ])
+    s <- whisker.render(script, p)
+    slurm_job(s, job_name, log_path, nnodes,
+              nproc, timelim, verbose)
+  }
+}
---
+++ tests/testthat/test.r
@@ -0,0 +1,15 @@
+context("test submission of jobs")
+
+test_that("prepare script", {
+  create_script(cmd="script\n", job_name="job", log_path="logpath")
+
+})
+
+
+test_that("", {
+#    slurm_job(script="hostname", job_name="test_slurm", 
+#                 log_path="..",
+#                 timelim="00:01:00", verbose=TRUE)
+    system("rm ../*.e*")
+    system("rm ../*.o*")
+})
