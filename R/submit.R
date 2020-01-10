a9da92cf37c2b2504c49c00f2bde7e8bab18edf5
Sinan Shi <sinan.shi@stats.ox.ac.uk>
2020-01-10 11:15:50 +0000

    adding job submission of bash

--- R/submit.R
+++ R/submit.R
@@ -14,57 +14,72 @@ NULL
 #' @return character the file name of the given full path
 #' @export
 get_name_from_path <- function(path) {
-  stopifnot(length(path) == 1)
-  name <- str_split(path, "/")[[1]]
-  return(name[length(name)])
+    stopifnot(length(path) == 1)
+    name <- str_split(path, "/")[[1]]
+    return(name[length(name)])
 }
 
 
 #' Create script from template
 create_script <- function(cmd, job_name, log_path, nnodes=1, nproc=1, 
                           timelim="480:00:00", template="slurm") {
-  param <- list()
-  param[["job_name"]] <- job_name
-  param[["log_path"]] <- log_path
-  param[["nnodes"]] <- nnodes
-  param[["nproc"]] <- nproc
-  param[["timelim"]] <- timelim
-  param[["cmd"]] <- cmd
-  script <- readLines(system.file(template, package="hpcr"))
-  script <- whisker.render(script, param)
-  return(script)
+    param <- list()
+    param[["job_name"]] <- job_name
+    param[["log_path"]] <- log_path
+    param[["nnodes"]] <- nnodes
+    param[["nproc"]] <- nproc
+    param[["timelim"]] <- timelim
+    param[["cmd"]] <- cmd
+    script <- readLines(system.file(template, package="hpcr"))
+    script <- whisker.render(script, param)
+    return(script)
 }
 
 
 slurm_job <- function(script, job_name, log_path=".", 
-                         nnodes=1, nproc=1, timelim="48:00:00", 
-                         verbose=FALSE) {
-  script <- create_script(cmd=script, job_name=job_name, 
-                          log_path=log_path, 
-                          nnodes=nnodes, nproc=nproc,
-                          timelim=timelim,
-                          template="slurm")
-  file <- tempfile()
-  writeLines(script, file)
-  if (verbose) {
-    f <- readLines(file)
-    cat(f, sep="\n")
-  }
+                      nnodes=1, nproc=1, timelim="48:00:00", 
+                      verbose=FALSE) {
+    script <- create_script(cmd=script, job_name=job_name, 
+                            log_path=log_path, 
+                            nnodes=nnodes, nproc=nproc,
+                            timelim=timelim,
+                            template="slurm")
+    file <- tempfile()
+    writeLines(script, file)
+    if (verbose) {
+        f <- readLines(file)
+        cat(f, sep="\n")
+    }
 
-  system2("sbatch", file)
+    system2("sbatch", file)
 }
 
 #' @export
 submit_slurm <- function(scirpt, param=NULL, job_name, log_path, 
                          nnodes=1, nproc=1, timelim="48:00:00", 
                          verbose=FALSE) {
-  if (is.null(param))
-    param <- data.table(dummy="")
+    if (is.null(param))
+        param <- data.table(dummy="")
 
-  for (i in seq(nrow(param))) {
-    p <- as.list(param[i, ])
-    s <- whisker.render(script, p)
-    slurm_job(s, job_name, log_path, nnodes,
-              nproc, timelim, verbose)
-  }
+    for (i in seq(nrow(param))) {
+        p <- as.list(param[i, ])
+        s <- whisker.render(script, p)
+        slurm_job(s, job_name, log_path, nnodes,
+                  nproc, timelim, verbose)
+    }
+}
+
+#' @export
+submit_bash <- function(script, param=NULL, verbose=FALSE) {
+    script <- whisker.render(script, param)
+
+    file <- tempfile()
+    writeLines(script, file)
+    if (verbose) {
+        f <- readLines(file)
+        cat(f, sep="\n")
+    }
+
+    res <- system2("bash", file, stdout=T)
+    return(res)
 }
--- tests/testthat/test.r
+++ tests/testthat/test.r
@@ -2,7 +2,6 @@ context("test submission of jobs")
 
 test_that("prepare script", {
   create_script(cmd="script\n", job_name="job", log_path="logpath")
-
 })
 
 
@@ -10,6 +9,11 @@ test_that("", {
 #    slurm_job(script="hostname", job_name="test_slurm", 
 #                 log_path="..",
 #                 timelim="00:01:00", verbose=TRUE)
-    system("rm ../*.e*")
-    system("rm ../*.o*")
+#    system("rm ../*.e*")
+#    system("rm ../*.o*")
+    script <- "echo {{text}}"
+    expect_equal(submit_bash(script, list(text="xx")), "xx")
 })
+
+
+
